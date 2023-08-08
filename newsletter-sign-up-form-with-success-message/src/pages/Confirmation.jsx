cfddef414ab09e96550799e06eca2b0e76819673
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-08-08 17:06:38 +0200

    added functioning button to Confirmation.jsx

--- newsletter-sign-up-form-with-success-message/src/pages/Confirmation.jsx
+++ newsletter-sign-up-form-with-success-message/src/pages/Confirmation.jsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import { useRouter } from 'next/navigation'
+
+
 export default function Confirmation(){
+    const router = useRouter();
+    const handler = () => {
+        router.push("/");
+    }
     return(
         <div>
-            <h1>Confirm!</h1>
+            <Image
+                src="/icon-success.svg"
+                width={200}
+                height={200}
+                alt="Success illustration"
+             />
+            <h1>Thanks for subscribing!</h1>
+            <p>A confirmation email has been sent to {}. Please open it and click the button inside to confirm your subscription.</p>
+            <button onClick={handler}>Dismiss message</button>
         </div>
     );
 }
