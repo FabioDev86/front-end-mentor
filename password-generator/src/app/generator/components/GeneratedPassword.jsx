3d3fb27ea89f01947b20956c55585a05e1c73eed
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-09-03 19:08:35 +0200

    styled GeneratedPassword

--- password-generator/src/app/generator/components/GeneratedPassword.jsx
+++ password-generator/src/app/generator/components/GeneratedPassword.jsx
@@ -5,20 +5,17 @@ export default function GeneratedPassword(props){
     
     return(
         <div className={styles.container}>
-            <h1>{props.password}</h1>
-            
-                <Image
-                    src="/copy-icon.png"
-                    width={35}
-                    height={40}
-                    alt="Copy to clipboard icon"
-                    onClick={() => {
-                        navigator.clipboard.writeText(props.password)
-                        alert("Password copied to clipboard");
-                    }}
-                />
-             
-            
+            <h1 id={styles.password}>{props.password}</h1>            
+            <Image
+                src="/copy-icon.png"
+                width={35}
+                height={40}
+                alt="Copy to clipboard icon"
+                onClick={() => {
+                    navigator.clipboard.writeText(props.password)
+                    alert("Password copied to clipboard");
+                }}
+            />          
         </div>
     );
 }
