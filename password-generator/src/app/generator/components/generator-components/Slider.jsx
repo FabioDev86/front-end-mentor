14209928e1cdf5540d23fc0338027762d0b95ed7
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-09-03 06:44:41 +0200

    changed label for slider

--- password-generator/src/app/generator/components/generator-components/Slider.jsx
+++ password-generator/src/app/generator/components/generator-components/Slider.jsx
@@ -4,9 +4,8 @@ export default function Slider(props){
     
     return(
         <div>
-            <label htmlFor="myRange">Character Length</label>
+            <label htmlFor="myRange">Character Length: {props.state}</label>
             <input type="range" min="1" max="30" value={props.state} onChange={(e) => {props.setState(e.target.value)}} id="myRange"></input>
-            <p>{props.state}</p>
         </div>
     );
 }
