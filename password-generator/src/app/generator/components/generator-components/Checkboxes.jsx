d462d3a42ce1073311823709a6f3f27b6cb34d6d
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-09-06 06:23:52 +0200

    completed checkboxes styling

--- password-generator/src/app/generator/components/generator-components/Checkboxes.jsx
+++ password-generator/src/app/generator/components/generator-components/Checkboxes.jsx
@@ -17,15 +17,15 @@ export default function Checkboxes(props){
             
             <input type="checkbox" id="lowercases" name="lowercases" checked={props.low[0]} onChange={() => {props.low[1](!props.low[0])}}></input>
             <label htmlFor="lowercases">Include Lowercase Letters</label>
-            <br></br>
+            
 
             <input type="checkbox" id="uppercases" name="uppercases" checked={props.upp[0]}  onChange={() => {props.upp[1](!props.upp[0])}}></input>
             <label htmlFor="uppercases">Include Uppercase Letters</label>
-            <br></br>
+            
 
             <input type="checkbox" id="numbers" name="numbers" checked={props.num[0]}  onChange={() => {props.num[1](!props.num[0])}}></input>
             <label htmlFor="numbers">Include Numbers</label>
-            <br></br>
+            
 
             <input type="checkbox" id="symbols" name="symbols" checked={props.sym[0]}  onChange={() => {props.sym[1](!props.sym[0])}}></input>
             <label htmlFor="symbols">Include Symbols</label>
