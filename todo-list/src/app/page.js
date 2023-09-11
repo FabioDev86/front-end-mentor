181eaf1b206d306fef67210f9e593d6a7e3c42d1
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-09-11 06:47:50 +0200

    created basic main structure

--- todo-list/src/app/page.js
+++ todo-list/src/app/page.js
@@ -1,9 +1,14 @@
+import Header from "./main-components/Header";
+import TaskAdder from "./main-components/TaskAdder";
+import TaskList from "./main-components/TaskList";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>TODO List</h1>
+      <Header />
+      <TaskAdder />
+      <TaskList />
     </main>
   )
 }
