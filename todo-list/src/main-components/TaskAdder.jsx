f38e9273468301c6959b59c7190b79c0897c8eb6
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-09-15 06:29:12 +0200

    completed TaskAdder style

--- todo-list/src/main-components/TaskAdder.jsx
+++ todo-list/src/main-components/TaskAdder.jsx
@@ -4,7 +4,7 @@ export default function TaskAdder(){
     return(
         <div className="flex justify-start bg-white dark:bg-slate-800 px-5 py-3 gap-3 rounded-lg">
             <Checkbox />
-            <p className="dark:text-slate-600">Create a new to do...</p>
+            <input placeholder="Create a new to do..." className="focus:outline-none border-none text-slate-800 w-full dark:text-slate-400 dark:bg-slate-800 "/>
         </div>
     );
 }
--- todo-list/src/utility-components/Checkbox.jsx
+++ todo-list/src/utility-components/Checkbox.jsx
@@ -1,6 +1,6 @@
 export default function Checkbox(){
     return(
-        <div className="w-[25px] h-[25px] border-2 rounded-[50%] dark:border-slate-700">            
+        <div className="w-[25px] h-[25px] shrink-0 border-2 rounded-[50%] dark:border-slate-700">            
         </div>
     );
 }
