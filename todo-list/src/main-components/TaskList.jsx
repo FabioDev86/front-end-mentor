93fbf3ac9f9532fdd19438ce0454395fb05b1f9a
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-09-16 06:28:37 +0200

    implemented dynamic list rendering

--- todo-list/src/main-components/TaskList.jsx
+++ todo-list/src/main-components/TaskList.jsx
@@ -1,13 +1,25 @@
 "use client";
 import { useTask } from "@/contexts/TaskContext";
 
+let tasklist = [];
+
 export default function TaskList(){
-    const {task} = useTask()
+    
+    const {task} = useTask();
+    
+    tasklist.push(task);
+    const listitem = tasklist.map(item => {
+        return(
+            <li key={item}>{item}</li>
+        );
+    });
 
     return(
         <div>
             <h1>Task List</h1>
-            <h1>{task}</h1>
+            <ul>
+                {listitem}
+            </ul>
         </div>
     );
 }
