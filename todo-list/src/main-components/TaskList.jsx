994e12e23124bdb4b60454f784b643c4234b56ff
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-09-19 06:36:40 +0200

    added state and onClick in Checkbox

--- todo-list/src/main-components/TaskList.jsx
+++ todo-list/src/main-components/TaskList.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useTask } from "@/contexts/TaskContext";
+import Checkbox from "@/utility-components/Checkbox";
 import Image  from "next/image";
 
 export default function TaskList(){    
@@ -13,7 +14,7 @@ export default function TaskList(){
             <ul>
                 {task.map((item, index) => {
                     return(
-                        <li key={index} className="flex">{item}<Image src="/icon-cross.svg" width={20} height={20} style={{height: '20px'}} /></li>
+                        <li key={index} className="flex"><Checkbox />{item}<Image src="/icon-cross.svg" width={20} height={20} style={{height: '20px'}} /></li>
                     );
                 })}
             </ul>
--- todo-list/src/utility-components/Checkbox.jsx
+++ todo-list/src/utility-components/Checkbox.jsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import { useState } from "react";
 
-export default function Checkbox(props){
-    console.log(props.ischecked);
+export default function Checkbox(){
+
+    const[checked, setChecked] = useState(false);
+
+    const handleClick = () => {
+        setChecked(!checked);
+    }
+
     return(
-        <div className={"w-[25px] h-[25px] shrink-0 border-2 rounded-[50%] pt-1 pl-1 dark:border-slate-700 " + (props.ischecked ? "bg-gradient-to-r from-violet-500 to-fuchsia-500 dark:from-sky-500" : "")} >            
-            {props.ischecked ? <Image width={15} height={15} src="/icon-check.svg" /> : ""}
+        <div onClick={handleClick} className={"w-[25px] h-[25px] shrink-0 border-2 rounded-[50%] pt-1 pl-1 dark:border-slate-700 " + (checked ? "bg-gradient-to-r from-violet-500 to-fuchsia-500 dark:from-sky-500" : "")} >            
+            {checked ? <Image width={15} height={15} src="/icon-check.svg" /> : ""}
         </div>
     );
 }
