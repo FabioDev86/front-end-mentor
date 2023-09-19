8c3c8122b4df8f5af80ea3ab4d240921fb2bda06
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-09-19 06:27:31 +0200

    styled Checkbox

--- todo-list/src/utility-components/Checkbox.jsx
+++ todo-list/src/utility-components/Checkbox.jsx
@@ -1,6 +1,10 @@
-export default function Checkbox(){
+import Image from "next/image";
+
+export default function Checkbox(props){
+    console.log(props.ischecked);
     return(
-        <div className="w-[25px] h-[25px] shrink-0 border-2 rounded-[50%] dark:border-slate-700">            
+        <div className={"w-[25px] h-[25px] shrink-0 border-2 rounded-[50%] pt-1 pl-1 dark:border-slate-700 " + (props.ischecked ? "bg-gradient-to-r from-violet-500 to-fuchsia-500 dark:from-sky-500" : "")} >            
+            {props.ischecked ? <Image width={15} height={15} src="/icon-check.svg" /> : ""}
         </div>
     );
 }
