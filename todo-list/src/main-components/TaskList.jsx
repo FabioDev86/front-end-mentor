f21353a6e42091b7b47b48bd94bcd9e81f073887
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-09-27 06:16:13 +0200

    added clear completed button

--- todo-list/src/main-components/TaskList.jsx
+++ todo-list/src/main-components/TaskList.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useTask } from "@/contexts/TaskContext";
 import List from "@/utility-components/List";
 import { useState } from "react";
 
@@ -8,6 +9,8 @@ export default function TaskList(){
     //This will store the type of the list selected by the user
     const [type, setType] = useState("general");
 
+    const {setCompletedTasks} = useTask();
+
     return(    
         <>
             <List type={type} />
@@ -15,6 +18,7 @@ export default function TaskList(){
                 <button className={type === "general" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("general")}}>All</button>
                 <button className={type === "active" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("active")}}>Active</button>
                 <button className={type === "completed" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("completed")}}>Completed</button>
+                <div><button className="text-gray-400 hover:text-black dark:hover:text-white" onClick={() =>{setCompletedTasks([])}}>Clear Completed</button></div>
             </div>  
         </>
     );        
