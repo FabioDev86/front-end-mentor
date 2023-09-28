bf6f14c7403e429ba4d1ca6071a74562b0e18c51
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-09-28 11:26:44 +0200

    installed react-responsive and updated NextJs

--- todo-list/src/main-components/TaskList.jsx
+++ todo-list/src/main-components/TaskList.jsx
@@ -4,6 +4,8 @@ import { useTask } from "@/contexts/TaskContext";
 import List from "@/utility-components/List";
 import { useState } from "react";
 
+import MediaQuery from "react-responsive"; 
+
 export default function TaskList(){    
     
     //This will store the type of the list selected by the user
@@ -12,15 +14,25 @@ export default function TaskList(){
     const {setCompletedTasks} = useTask();
 
     return(    
-        <>
-            <List type={type} />
-            <div className="flex justify-around">
-                <button className={type === "general" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("general")}}>All</button>
-                <button className={type === "active" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("active")}}>Active</button>
-                <button className={type === "completed" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("completed")}}>Completed</button>
-                
-            </div>
-            <button className="text-gray-400 hover:text-black dark:hover:text-white" onClick={() =>{setCompletedTasks([])}}>Clear Completed</button>  
-        </>
+        <div>
+            <MediaQuery maxWidth={375}>
+                <List type={type} />
+                <div className="flex justify-around">
+                    <button className={type === "general" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("general")}}>All</button>
+                    <button className={type === "active" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("active")}}>Active</button>
+                    <button className={type === "completed" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("completed")}}>Completed</button>
+                </div>
+                <button className="text-gray-400 hover:text-black dark:hover:text-white" onClick={() =>{setCompletedTasks([])}}>Clear Completed</button>  
+            </MediaQuery>
+            <MediaQuery minWidth={376}>
+                <List type={type} />
+                <div className="flex justify-around">
+                    <button className={type === "general" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("general")}}>All</button>
+                    <button className={type === "active" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("active")}}>Active</button>
+                    <button className={type === "completed" ? "text-cyan-500": "text-gray-400 hover:text-black dark:hover:text-white"} onClick={() => {setType("completed")}}>Completed</button>
+                    <button className="text-gray-400 hover:text-black dark:hover:text-white" onClick={() =>{setCompletedTasks([])}}>Clear Completed</button>  
+                </div>
+            </MediaQuery>
+        </div>
     );        
 }
