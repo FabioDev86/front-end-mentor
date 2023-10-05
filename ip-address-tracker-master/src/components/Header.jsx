c4f7afd9ef4752ca9485d764da5bde3c4869739d
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-10-05 17:45:59 +0200

    styled Header

--- ip-address-tracker-master/src/components/Header.jsx
+++ ip-address-tracker-master/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 export default function Header(){
     return(
-        <div className='flex flex-col items-center'> 
-            <h1 className="text-white">Ip Address Tracker</h1>
+        <div className='flex flex-col items-center gap-10 p-10'> 
+            <h1 className="text-white text-3xl font-bold	">IP Address Tracker</h1>
             <div className='flex justify-center'>
-                <input placeholder="Search for any IP address or domain" className='p-3 w-[30rem]'></input>
-                <button className='bg-black py-3 px-5 rounded-r-lg'><Image
+                <input placeholder="Search for any IP address or domain" className='rounded-l-lg p-3 w-[30rem]'></input>
+                <button className='bg-black py-3 px-5 rounded-r-lg'>
+                        <Image
                             src="/icon-arrow.svg"
                             width={10}
                             height={10}
