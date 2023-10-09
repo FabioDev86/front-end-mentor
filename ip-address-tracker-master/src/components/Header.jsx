e5539400553c4f7a0485a963f53905795667fe21
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-10-09 08:12:54 +0200

    styled focus and valid input

--- ip-address-tracker-master/src/components/Header.jsx
+++ ip-address-tracker-master/src/components/Header.jsx
@@ -1,19 +1,35 @@
+'use client'
+
 import Image from 'next/image'
+import { useState } from 'react';
+
 export default function Header(){
+    const [ip, setIp] = useState();
+
+    const [valid, setValid] = useState(false);
+    const [focus, setFocus] = useState(false);
+
     return(
         <div className='flex flex-col items-center gap-10 p-10'> 
-            <h1 className="text-white text-3xl font-bold	">IP Address Tracker</h1>
-            <div className='flex justify-center'>
-                <input placeholder="Search for any IP address or domain" className='rounded-l-lg p-3 w-[30rem]'></input>
-                <button className='bg-black py-3 px-5 rounded-r-lg'>
+            <h1 className="text-white text-3xl font-bol">IP Address Tracker</h1>
+            <form className='flex flex-col'>
+                
+                <div className='flex'>
+                    <input id="ip_address" onFocus={() => {setFocus(true)}} onBlur={() => {setFocus(false)}} onChange={(e) => {setIp(e.target.value)}} placeholder="Search for any IP address or domain" className={'rounded-l-lg p-3 w-[30rem] focus:outline-none ' + (valid ? '' : 'focus:bg-red-400 focus:placeholder-gray-800')}></input>
+                    <button className='bg-black py-3 px-5 rounded-r-lg'>
                         <Image
                             src="/icon-arrow.svg"
                             width={10}
                             height={10}
                             alt="Arrow"
                         />
-                </button>
-            </div>
+                    </button>                    
+                </div>
+                <label htmlFor='ip_address' className={valid ? 'invisible' : focus ? 'text-red-600' : 'invisible'}>Insert a valid IP address</label>
+                
+                
+            </form>
+            <h1>{ip}</h1>
         </div>
     );
 }
