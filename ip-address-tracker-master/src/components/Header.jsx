3a90f4f59fbdea4df191f7f6101122748ed946cb
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-10-09 08:24:49 +0200

    created utility function check_valid_ip()

--- ip-address-tracker-master/src/components/Header.jsx
+++ ip-address-tracker-master/src/components/Header.jsx
@@ -12,8 +12,7 @@ export default function Header(){
     return(
         <div className='flex flex-col items-center gap-10 p-10'> 
             <h1 className="text-white text-3xl font-bol">IP Address Tracker</h1>
-            <form className='flex flex-col'>
-                
+            <form className='flex flex-col'>                
                 <div className='flex'>
                     <input id="ip_address" onFocus={() => {setFocus(true)}} onBlur={() => {setFocus(false)}} onChange={(e) => {setIp(e.target.value)}} placeholder="Search for any IP address or domain" className={'rounded-l-lg p-3 w-[30rem] focus:outline-none ' + (valid ? '' : 'focus:bg-red-400 focus:placeholder-gray-800')}></input>
                     <button className='bg-black py-3 px-5 rounded-r-lg'>
@@ -25,11 +24,8 @@ export default function Header(){
                         />
                     </button>                    
                 </div>
-                <label htmlFor='ip_address' className={valid ? 'invisible' : focus ? 'text-red-600' : 'invisible'}>Insert a valid IP address</label>
-                
-                
+                <label htmlFor='ip_address' className={valid ? 'invisible' : focus ? 'text-red-600' : 'invisible'}>Insert a valid IP address</label>                
             </form>
-            <h1>{ip}</h1>
         </div>
     );
 }
