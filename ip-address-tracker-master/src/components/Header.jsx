d9b3889d325678416ebe150d84f4e38f36b6ea7a
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-10-24 18:27:16 +0200

    added responsive Header

--- ip-address-tracker-master/src/components/Header.jsx
+++ ip-address-tracker-master/src/components/Header.jsx
@@ -27,11 +27,11 @@ export default function Header(){
     }
 
     return(
-        <div className='flex flex-col items-center gap-10 p-[4rem]'> 
+        <div className='flex flex-col items-center gap-10 p-[4rem] max-[660px]:px-[1rem]'> 
             <h1 className="text-white text-3xl font-bol">IP Address Tracker</h1>
             <form className='flex flex-col' onSubmit={handleSubmit}>                
                 <div className='flex'>
-                    <input id="ip_address" onFocus={() => {setFocus(true)}} onBlur={() => {setFocus(false)}} onChange={handleChange} placeholder="Search for any IP address or domain" className={'rounded-l-lg p-3 w-[30rem] focus:outline-none ' + (valid ? '' : 'focus:bg-red-400 focus:placeholder-gray-800')}></input>
+                    <input id="ip_address" onFocus={() => {setFocus(true)}} onBlur={() => {setFocus(false)}} onChange={handleChange} placeholder="Search for any IP address or domain" className={'rounded-l-lg p-3 w-[60vw] focus:outline-none ' + (valid ? '' : 'focus:bg-red-400 focus:placeholder-gray-800')}></input>
                     <button className='bg-black py-3 px-5 rounded-r-lg' type='submit'>
                         <Image
                             src="/icon-arrow.svg"
