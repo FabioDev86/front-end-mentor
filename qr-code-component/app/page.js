8b1c8aaf1fd27dc15eafecc5065fa0df53c931a7
Utente <fabio.bauleo.developer@gmail.com>
2024-11-01 07:34:37 +0100

    customized font size

--- qr-code-component/app/page.js
+++ qr-code-component/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-lightgray">
       <h1>QR Code</h1>
+      <p>This paragaraph should be 15px</p>
     </main>
   );
 }
